4eb8c2dbb4b244b238c3218f3deb83cda56f3366
Ashfaqul Asif <asif@itconquest.com>
2025-07-21 18:30:54 +0600

    added project.

---
+++ frontend/src/navigation/vertical/index.js
@@ -0,0 +1,146 @@
+export function getVerticalNavItems(role) {
+  if (role === 'admin') {
+    return [
+      {
+        title: 'Dashboard',
+        to: { name: 'admin' },
+        icon: { icon: 'tabler-smart-home' },
+      },
+      {
+        title: 'Users',
+        to: { name: 'AdminUsers' },
+        icon: { icon: 'tabler-users' },
+      },
+      {
+        title: 'Reports',
+        to: { name: 'AdminReports' },
+        icon: { icon: 'tabler-report' },
+      },
+    ]
+  } else if (role === 'business') {
+    return [
+      {
+        title: 'Connect Your Business',
+        to: { name: 'BusinessConnect' },
+        icon: { icon: 'tabler-link' },
+      },
+      {
+        title: 'Dashboard',
+        to: { name: 'business' },
+        icon: { icon: 'tabler-smart-home' },
+      },
+      {
+        title: 'Appointments',
+        icon: { icon: 'tabler-calendar' },
+        children: [
+          {
+            title: 'Calendar',
+            to: { name: 'BusinessAppointmentsCalendar' },
+            icon: { icon: 'tabler-calendar-event' },
+          },
+          {
+            title: 'Settings',
+            to: { name: 'BusinessAppointmentsSettings' },
+            icon: { icon: 'tabler-settings' },
+          },
+        ],
+      },
+      {
+        title: 'Business Settings',
+        icon: { icon: 'tabler-activity' },
+        children: [
+          {
+            title: 'Opening hours',
+            to: { name: 'BusinessActivityTimes' },
+            icon: { icon: 'tabler-clock' },
+          },
+          {
+            title: 'Team',
+            to: { name: 'BusinessTeamMembers' },
+            icon: { icon: 'tabler-users-group' },
+          },
+          {
+            title: 'Services Menu',
+            to: { name: 'BusinessServicesOffered' },
+            icon: { icon: 'tabler-briefcase' },
+          },
+          {
+            title: 'Scheduled shifts',
+            to: { name: 'BusinessTeamAvailability' },
+            icon: { icon: 'tabler-calendar-time' },
+          },
+        ],
+      },
+      {
+        title: 'Clients',
+        icon: { icon: 'tabler-user' },
+        children: [
+          {
+            title: 'List of All Customers',
+            to: { name: 'BusinessCustomersList' },
+            icon: { icon: 'tabler-list-details' },
+          },
+          {
+            title: 'Customers who Spent the Most',
+            to: { name: 'BusinessTopCustomers' },
+            icon: { icon: 'tabler-crown' },
+          },
+        ],
+      },
+      {
+        title: 'Marketing',
+        icon: { icon: 'tabler-crown' },
+        children: [
+          {
+            title: 'Generate QR codes & Activity Links',
+            to: { name: 'BusinessMarketingQR' },
+            icon: { icon: 'tabler-qrcode' },
+          },
+          {
+            title: 'Extra SMS & Email Services',
+            to: { name: 'BusinessMarketingSMS' },
+            icon: { icon: 'tabler-mail' },
+          },
+          {
+            title: 'Reminder for Customers',
+            to: { name: 'BusinessMarketingReminders' },
+            icon: { icon: 'tabler-bell' },
+          },
+        ],
+      },
+      {
+        title: 'Plan',
+        icon: { icon: 'tabler-credit-card' },
+        children: [
+          {
+            title: 'Monthly Plan Routes',
+            to: { name: 'BusinessPlanMonthly' },
+            icon: { icon: 'tabler-calendar-month' },
+          },
+          {
+            title: 'Invoice Management & Downloads',
+            to: { name: 'BusinessPlanInvoices' },
+            icon: { icon: 'tabler-file-invoice' },
+          },
+        ],
+      },
+      {
+        title: 'Support',
+        icon: { icon: 'tabler-headset' },
+        children: [
+          {
+            title: 'Internal Chat',
+            to: { name: 'BusinessSupportChat' },
+            icon: { icon: 'tabler-message' },
+          },
+          {
+            title: 'Whatsapp by Chatbookly',
+            to: { name: 'BusinessSupportWhatsapp' },
+            icon: { icon: 'tabler-brand-whatsapp' },
+          },
+        ],
+      },
+    ]
+  }
+  return []
+}
